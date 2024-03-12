e3920ccdad041ce37e41cda80354ade63b4c09e2
Alex ATC <alex.atc.dev@gmail.com>
2024-03-12 01:42:56 +0100

    init: Basic feature app + config shadcn

---
+++ README.md
@@ -0,0 +1,12 @@
+### Etapes :
+
+[X] : VITE React Typescript
+[X] : ShadCN + tailwind
+[] : React-router
+[] : dotenv
+
+[] : Jest
+[] : Redux Toolkit / Zustand
+
+[] : Composant réutillisable Shadcn :
+    [X] : Button
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button>Button Shadcn
+         </Button>
+        <p className="logo">
+          Starter by Alex ATC 
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
